399b04c420d765485a4d47d274dd0d38652ca363
miguellucass <140181208+miguellucass@users.noreply.github.com>
2026-05-23 19:08:25 -0300

    Commit Instalações

--- App.js
+++ App.js
@@ -1,21 +1,66 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { getAuth } from './src/firebaseConfig.js';
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword }
+from 'firebase/auth';
+import { signInWithEmailAndPassword }
+from 'firebase/auth';
+export default function App(){
+  const criarConta = () => {
+    createUserWithEmailAndPassword(
+      getAuth,
+    )
+    .then((userCredential) => {
+      console.log('Usuário criado!');
+      console.log(userCredential.user);
+    })
+    .catch((error) => {
+      console.log('Erro');
+      console.log(error);
+    });
+  };
+  const login = () => {
+    signInWithEmailAndPassword(
+      getAuth,
+    )
+    .then((userCredential) => {
+      console.log('Login realizado!');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <text>Hello World</text>
+      <Button 
+        title="Criar Conta" onPress={criarConta}
+      />
+      <Button 
+        title="Login" onPress={login}
+      />
+    </View>
+  );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Open up App.js to start working on your app!</Text>
+      <Text>Hello, World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 100,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
 });
---
+++ src/firebaseConfig.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+import { db } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBT6ESb7T4FBHgesnCDKxb8siqIdSTRtjA",
+  authDomain: "help-2939b.firebaseapp.com",
+  projectId: "help-2939b",
+  storageBucket: "help-2939b.firebasestorage.app",
+  messagingSenderId: "388394047174",
+  appId: "1:388394047174:web:0447fb28d03ec76c4f29c7",
+  measurementId: "G-5T7SYRDNCY"
+};
+
+const initializeApp = initializeApp(firebaseConfig);
+const getAnalytics = getAnalytics(initializeApp);
+const getAuth = getAuth(initializeApp);
+const db = getFirestore(initializeApp);
+// Initialize Firebase
+export const initializeApp = initializeApp(firebaseConfig);
+export const firebaseConfig = firebaseConfig;
+export const getAnalytics = getAnalytics(initializeApp);
+export const getAuth = getAuth(initializeApp);
+export const db = getFirestore(initializeApp);
